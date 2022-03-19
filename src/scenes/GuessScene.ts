65c9cfb3e056bfbc97514f208bd1f8f9fd9d21f9
EddieLevin <edlevin@udel.edu>
2022-03-19 16:38:44 -0400

    added comments to GuessScene

--- src/scenes/GuessScene.ts
+++ src/scenes/GuessScene.ts
@@ -12,6 +12,7 @@ export default class GuessScene extends Phaser.Scene {
         super("guess");
     }
 
+    // calls diferent functions depending on what kind of object is clicked 
     handleInteract(pointer: Phaser.Input.Pointer, gameObject: Phaser.GameObjects.Text) {
         if (gameObject.type != "Text") { return }
         if (Object.keys(this.keywords).includes(gameObject.text)) {
@@ -22,6 +23,8 @@ export default class GuessScene extends Phaser.Scene {
         }
     }
 
+    // when a keyword is clicked, that keyword is appended to the list of current guesses
+    //  if the keyword is part of the guess, it is removed from the list
     appendGuess(keyword: Phaser.GameObjects.Text) {
         if (this.currentGuess.includes(keyword.text)) {
             this.currentGuess.splice(this.currentGuess.indexOf(keyword.text), 1)
@@ -35,6 +38,8 @@ export default class GuessScene extends Phaser.Scene {
         this.guessText.setColor("White")
     }
 
+    // when submit is clicked, the guess is compared to the opponent's password, and the text color is changed accordingly,
+    //  and then the turn is switched to the opponent and the current guess is cleared
     submit() {
         if (Player.otherPlayer.guessPassword(this.currentGuess)) {
             this.guessText.setColor("Green")
@@ -52,6 +57,9 @@ export default class GuessScene extends Phaser.Scene {
     }
 
     create() {
+
+        // TODO: create and assign these players in the password creation phase
+        //       Look at Player.ts to see how to use it
         var player1 = new Player(1)
         var player2 = new Player(2)
         player1.setPassword(['123'])
@@ -59,10 +67,15 @@ export default class GuessScene extends Phaser.Scene {
         Player.activePlayer = player1
         Player.otherPlayer = player2
 
+        // WARNING: if the text in the submit button is chnaged, handleInteract must also be changed
         this.guessText = this.add.text(0, 150, "Guess: " + this.currentGuess.toString())
         this.add.text(0, 200, "submit").setInteractive()
         this.turnText = this.add.text(150, 0, "Player " + Player.activePlayer.id + "'s Turn").setFontSize(12)
 
+        // TODO:rewrite this keyword creation to involve less hard coded values
+        //      and have automatic formating 
+        //      (and maybe change the text objects to containers)
+        // WARNING: currently, the strings used for the key must match the text exactly
         this.keywords['abc'] = this.add.text(20, 20, "abc");
         this.keywords['123'] = this.add.text(20, 20, "123").setPosition(70, 70);
         this.keywords['Test'] = this.add.text(20, 20, "Test").setPosition(0, 80);
