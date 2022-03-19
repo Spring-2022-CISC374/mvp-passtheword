82aa4f6c4511912031efe0894d60ca308e3f598c
EddieLevin <edlevin@udel.edu>
2022-03-18 22:31:56 -0400

    added submit functionality

--- src/scenes/GuessScene.ts
+++ src/scenes/GuessScene.ts
@@ -1,4 +1,5 @@
 import Phaser from 'phaser'
+import { text } from 'stream/consumers'
 
 class player{
     constructor(id: number){
@@ -13,7 +14,12 @@ class player{
         this.password = input
     }
     guessPassword(input: string[]){
-        return (this.password == input)
+        if(this.password.length != input.length) {return false}
+        for(var i = 0; i < input.length; i++){
+            if(this.password[i] != input[i]){return false}
+        }
+
+        return true
     }
 }
 
@@ -29,27 +35,46 @@ export default class GuessScene extends Phaser.Scene {
         super("guess");
     }
 
-    appendGuess(pointer: Phaser.Input.Pointer, gameObject: Phaser.GameObjects.Text){
-        if(this.currentGuess.includes(gameObject.text)){
-            this.currentGuess.splice(this.currentGuess.indexOf(gameObject.text),1)
+    handleInteract(pointer: Phaser.Input.Pointer, gameObject: Phaser.GameObjects.Text){
+        if(gameObject.type != "Text"){ return }
+        if(Object.keys(this.keywords).includes(gameObject.text)){
+            this.appendGuess(gameObject)
+        }
+        if(gameObject.text == "submit"){
+            this.submit()
+        }
+    }
+
+    appendGuess(keyword: Phaser.GameObjects.Text){
+        if(this.currentGuess.includes(keyword.text)){
+            this.currentGuess.splice(this.currentGuess.indexOf(keyword.text),1)
         }
         else{
-            this.currentGuess.push(gameObject.text)
+            this.currentGuess.push(keyword.text)
         }
-        this.updateGuessText()
-    }
-    updateGuessText(){
         this.guessText.setText("Guess: " + this.currentGuess.toString())
-        this.guessText.setPosition(0,150)
-        console.log(this.currentGuess)
+        this.guessText.setColor("White")
+    }
+
+    submit(){
+        console.log(this.otherPlayer.guessPassword(this.currentGuess))
+        if(this.otherPlayer.guessPassword(this.currentGuess)){
+            this.guessText.setColor("Green")
+        }
+        else{
+            this.guessText.setColor("Red")
+        }
     }
 
     create(){
         this.activePlayer = new player(1)
         this.otherPlayer = new player(2)
-        this.activePlayer.setPassword(["abc","123"])
+        this.activePlayer.setPassword(['123'])
+        this.otherPlayer.setPassword(['abc','123'])
 
         this.guessText = this.add.text(0,150,"Guess: " + this.currentGuess.toString())
+
+        this.add.text(0,200,"submit").setInteractive()
 
         this.keywords['abc'] = this.add.text(20,20,"abc");
         this.keywords['123'] = this.add.text(20,20,"123").setPosition(70,70);
@@ -57,7 +82,7 @@ export default class GuessScene extends Phaser.Scene {
             text.setInteractive()
         }
 
-        this.input.on('gameobjectdown',this.appendGuess, this)
+        this.input.on('gameobjectdown',this.handleInteract, this)
 
     }
 
