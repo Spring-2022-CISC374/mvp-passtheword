4c3f5ef0a031de65f689014ef09b1aa0d13bdcaf
stonex159 <stonex@udel.edu>
2022-03-21 19:17:29 -0400

    deleted keyword class for real this time

--- src/keyword.ts
+++
@@ -1,7 +0,0 @@
-import Phaser from "phaser";
-export class Keyword extends Phaser.GameObjects.Text{
-    constructor(scene, x, y, text, style){
-        super(scene, x, y, text, style);
-        this.setInteractive();
-    }
-}
