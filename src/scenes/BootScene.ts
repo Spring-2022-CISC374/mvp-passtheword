42f0a0d2765052360f70c5e9a2cafdcfbe86dc89
stonex159 <stonex@udel.edu>
2022-03-21 19:16:43 -0400

    got rid of unnecessary preloading

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -6,38 +6,11 @@ export default class BootScene extends Phaser.Scene {
     }
 
     preload(){
-        this.load.image("background", '../images/background.png');
-        this.load.spritesheet("explosion", '../spritesheets/explosion.png', {frameWidth: 16, frameHeight: 16});
-        this.load.spritesheet("orb", "../spritesheets/power-up.png", {frameWidth: 16, frameHeight: 16});
-        this.load.image("player", '../images/paddle1.png');
+        
     }
     
     create() {
         this.add.text(20,20,"Loading game...");
-
-
-        this.anims.create({
-            key: "red",
-            frames: this.anims.generateFrameNumbers("orb", {start: 0, end: 1}),
-            frameRate: 5,
-            repeat: -1,
-        });
-
-        this.anims.create({
-            key: "gray",
-            frames: this.anims.generateFrameNumbers("orb", {start: 2, end: 3}),
-            frameRate: 5,
-            repeat: -1,
-        });
-
-        this.anims.create({
-            key: "explode",
-            frames: this.anims.generateFrameNumbers("explosion", {}),
-            frameRate: 20,
-            repeat: 0,
-            hideOnComplete: true
-        });
-
 
         this.scene.start("MainMenu");
     }
