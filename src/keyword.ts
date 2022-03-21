a7492c7b52af554cde673f53544cdaa55ab60319
stonex159 <stonex@udel.edu>
2022-03-21 19:16:25 -0400

    Got rid of keyword class

--- src/keyword.ts
+++ src/keyword.ts
@@ -1,6 +1,7 @@
-export class Keyword {
-    constructor(text: string){
-        this.text = text;
+import Phaser from "phaser";
+export class Keyword extends Phaser.GameObjects.Text{
+    constructor(scene, x, y, text, style){
+        super(scene, x, y, text, style);
+        this.setInteractive();
     }
-    text: string
 }
