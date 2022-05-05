1447613334f202df141e141f1deab13f8a2a7c9f
edlevin <45210765+EddieLevin@users.noreply.github.com>
2022-05-04 22:53:30 -0400

    Updates

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-importScripts("/mvp-passtheword/devprecache-manifest.1d02d501afad50d3c68e82913028492e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/mvp-passtheword/devprecache-manifest.65e939f33810380f0f8163edd71aac0e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
