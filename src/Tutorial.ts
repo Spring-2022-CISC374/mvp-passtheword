98366598f54ffd56c071a40c06d2576f75bb8302
stonex159 <stonex@udel.edu>
2022-05-13 16:22:47 -0400

    Fixed text not showing, ShopTutorial iamge added

--- src/Tutorial.ts
+++ src/Tutorial.ts
@@ -3,7 +3,8 @@ import { size } from './app';
 
 export class Tutorial extends Phaser.Scene {
 
-    gameText: string[] = []
+    private gameText: Phaser.GameObjects.Text
+    private tutorialImage: Phaser.GameObjects.Image
 
     constructor(){
         super("tutorial");
@@ -25,16 +26,15 @@ export class Tutorial extends Phaser.Scene {
             //this.gameText['Color explanation'] = this.add.text(size.x/2, 70, "If one of the keywords you used becomes red, then it is not in your opponent's password.").setOrigin().setVisible(false);
             //this.gameText['Color explanation 2'] = this.add.text(size.x/2, 90, "If one of the keywords you used becomes yellow, then it is in your opponent's password but not where you guessed.").setOrigin().setVisible(false);    
             //this.gameText['Color explanation 3'] = this.add.text(size.x/2, 110, "If one of the keywords you used becomes green, then it is in your opponent's password and you guessed where it is correctly.").setOrigin().setVisible(false);
-            var style = { fontFamily: 'Arial', color: 'white', align: 'center', wordWrap: { width: size.x+20, useAdvancedWrap: true }};
+            var style = { fontFamily: 'Arial', color: 'white', align: 'center', wordWrap: { width: size.x-40, useAdvancedWrap: true }};
             // Quick Greeting
-            this.gameText['Greeting'] = this.add.text(size.x/2,70,"Welcome players. This is just a quick tutorial to explain some things that aren't obvious. Click to see more.", style).setOrigin().setVisible(true).setFontSize(15);
+            this.gameText = this.add.text(size.x/2,70,"Welcome players. This is just a quick tutorial to explain some things that aren't obvious. Click to see more.", style).setOrigin().setVisible(true).setFontSize(15);
             // Shop & PowerUps
-            this.gameText['Power Up eExplanation'] = this.add.text(size.x, 70, "This is the shop. Here you can click a power up to buy it and get a leg up on your opponent.", style).setOrigin().setVisible(false).setFontSize(15);
-            this.gameText['Gaining Money'] = this.add.text(size.x, 110, "Every time you guess part of the opponent's password they get money for the shop. $1 for each keyword you guess correctly.", style).setOrigin().setVisible(false).setFontSize(15);
+           
             // Remember
-            this.gameText['Remember Password Explanation'] = this.add.text(size.x, 70, "Every 3 turns you will be shown this screen. If you remember your password and type it correctly, everything in the shop becomes cheaper.", style).setOrigin().setVisible(false).setFontSize(15);
+            
             // Send Off
-            this.gameText['Send Off'] = this.add.text(size.x, 70, "When you are ready click once more to start the game.", style).setOrigin().setVisible(false).setFontSize(15);
+            
 
         // Pictures
         
@@ -55,18 +55,17 @@ export class Tutorial extends Phaser.Scene {
         this.input.on('pointerdown', function (pointer) {
             count++
             if(count == 1){
-                this.gameText['Greeting'].setVisible(false);
-                this.gameText['Power Up Explanation'].setVisible(true);
-                this.gameText['Gaining Money'].setVisible(true);
+                this.gameText.setVisible(false);
+                this.gameText = this.add.text(size.x/2, 70, "This is the shop. Here you can click a power up to buy it and get a leg up on your opponent.\nEvery time you guess part of the opponent's password they get money for the shop. $1 for each keyword you guess correctly.", style).setOrigin().setFontSize(15);
+                this.add.image(size.x/2, size.y/2, 'shopTutorial').setOrigin();
             }
             if(count == 2){
-                this.gameText['Power Up Explanation'].setVisible(false);
-                this.gameText['Gaining Money'].setVisible(false);
-                this.gameText['Remember Password Explanation'].setVisible(true);
+                this.gameText.setVisible(false);
+                this.gameText = this.add.text(size.x/2, 70, "Every 3 turns you will be shown this screen. If you remember your password and type it correctly, everything in the shop becomes cheaper.", style).setOrigin().setFontSize(15);
             }
-            if(count == 1){
-                this.gameText['Remember Password Explanation'].setVisible(false);
-                this.gameText['Send Off'].setVisible(true);
+            if(count == 3){
+                this.gameText.setVisible(false);
+                this.gameText= this.add.text(size.x/2, 70, "When you are ready click once more to start the game.", style).setOrigin().setFontSize(15);
             }
             if(count == 4)
                 this.scene.start('transition');
